71e65ca7ceb911a9b366808942a8a6a719d433b7
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-09-08 14:40:08 +0530

    fetch data with useQuery

--- src/react-query/RQSuperHeroes.jsx
+++ src/react-query/RQSuperHeroes.jsx
@@ -1,8 +1,28 @@
+import axios from 'axios';
 import React from 'react'
+import { useQuery } from 'react-query';
 
 const RQSuperHeroes = () => {
+    const {isLoading, data} = useQuery('super-heroes', () => {
+        return axios.get("http://localhost:4000/superheroes")
+    });
+
+    if(isLoading){
+        return <h2 className='my-3'>Loading...</h2>
+    }
+
   return (
-    <div>RQSuperHeroes</div>
+    <div>
+        <h2 className='my-3'>Super Heroes</h2>
+        {
+            data?.data.map((hero) => (
+                <div className='text-center' key={hero.id}>
+                    <span>Hero: {hero.name}</span>
+                    <p>Alterego: {hero.alterego}</p>
+                </div>
+            ))
+        }
+    </div>
   )
 }
 
--- src/react-query/ReactQuery.jsx
+++ src/react-query/ReactQuery.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { Link, Outlet } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 const ReactQuery = () => {
   return (
-    <div className='d-flex gap-4 flex-column align-items-center'>
-      <div className='d-flex gap-3 justify-content-center'>
-        <Link to='superheroes'><button className='btn btn-dark'>Super Heroes</button></Link>
-        <Link to='rqsuperheroes'><button className='btn btn-dark'>RQ Super Heroes</button></Link>
+    <QueryClientProvider client={queryClient}>
+      <div className='d-flex gap-4 flex-column align-items-center'>
+        <div className='d-flex gap-3 justify-content-center'>
+          <Link to='superheroes'><button className='btn btn-dark'>Super Heroes</button></Link>
+          <Link to='rqsuperheroes'><button className='btn btn-dark'>RQ Super Heroes</button></Link>
+        </div>
+        <div>
+          <Outlet />
+        </div>
       </div>
-      <div>
-        <Outlet />
-      </div>
-    </div>
+    </QueryClientProvider>
   )
 }
 
